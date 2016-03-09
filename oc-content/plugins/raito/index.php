be2b5ba6cab5c09297cffc8c2bb7dbff8f31770b
sugardrunk <jannekahkonen@gmail.com>
2016-03-09 17:56:41 +0200

    add empty placeholder for the plugin using routes

---
+++ oc-content/plugins/raito/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Raito
+Plugin URI: https://github.com/gaissa/Raito
+Description: TODO
+Version: 0.0.1
+Author: Janne Kähkönen
+Author URI: https://github.com/gaissa/
+Short Name: raito
+Plugin update URI: raito
+*/
+
+
+    /**
+    *  Routes Documentation on the wiki : http://doc.osclass.org/Using_routes_in_Osclass
+    */
+
+?>
