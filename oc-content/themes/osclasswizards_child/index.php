76233a27e4530279e3ff10b9b1fab8f9afa4db34
sugardrunk <jannekahkonen@gmail.com>
2016-03-15 00:02:06 +0200

    add files to child theme

--- oc-content/themes/osclasswizards_child/index.php
+++ oc-content/themes/osclasswizards_child/index.php
@@ -4,8 +4,8 @@ Theme Name: Raito
 Theme URI: #
 Description: OsclassWizards Child Theme
 Version: 0.0.1
-Author: #
-Author URI: #
+Author: Janne Kähkönen
+Author URI: https://github.com/gaissa
 Parent Theme: osclasswizards
 */
 ?>
