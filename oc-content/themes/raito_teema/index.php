6e81f11cbf151a8c8206ccf3208fceffc0899172
sugardrunk <jannekahkonen@gmail.com>
2016-04-07 18:45:51 +0300

    add index.php to the Raito theme

---
+++ oc-content/themes/raito_teema/index.php
@@ -0,0 +1,42 @@
+<?php
+    /*
+     *      Osclass – software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2014 OSCLASS
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+	 
+/*
+Theme Name: Raito
+Theme URI: #
+Description: A simple and clean responsive theme. Based on OsclassWizards Free Responsive Theme
+Version: 0.0.1
+Author: Janne Kähkönen
+Author URI: #
+Widgets:  header, footer
+Theme update URI: #
+*/
+    function raito_teema_theme_info() {
+        return array(
+             'name'        => 'raito_teema'
+            ,'version'     => '0.0.1'
+            ,'description' => 'A simple and clean responsive theme. Based on OsclassWizards Free Responsive Theme'
+            ,'author_name' => 'Janne Kähkönen'
+            ,'author_url'  => '#'
+            ,'locations'   => array('header', 'footer')
+        );
+    }
+?>
