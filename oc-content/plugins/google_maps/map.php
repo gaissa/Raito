85cbbe5d25126a39f1e1eb9d1d94fef3770ba9d1
sugardrunk <jannekahkonen@gmail.com>
2016-04-07 16:48:16 +0300

    remove regions from the map

--- oc-content/plugins/google_maps/map.php
+++ oc-content/plugins/google_maps/map.php
@@ -1,4 +1,5 @@
 <div id="itemMap" style="width: 100%; height: 240px;"></div>
+
 <?php if($item['d_coord_lat'] != '' && $item['d_coord_long'] != '') {?>
     <script type="text/javascript">
         var latlng = new google.maps.LatLng(<?php echo $item['d_coord_lat']; ?>, <?php echo $item['d_coord_long']; ?>);
@@ -16,10 +17,10 @@
         });
     </script>
 <?php } else { ?>
-    <script type="text/javascript">
+    <script type="text/javascript"> 
         var map = null;
         var geocoder = null;
-
+     
         var myOptions = {
             zoom: 13,
             center: new google.maps.LatLng(37.4419, -122.1419),
@@ -29,7 +30,7 @@
 
         map = new google.maps.Map(document.getElementById("itemMap"), myOptions);
         geocoder = new google.maps.Geocoder();
-
+     
         function showAddress(address) {
             if (geocoder) {
                 geocoder.geocode( { 'address': address}, function(results, status) {
@@ -39,7 +40,7 @@
                             map: map,
                             position: results[0].geometry.location
                         });
-                        marker.setMap(map);
+                        marker.setMap(map);  
                     } else {
                         $("#itemMap").remove();
                     }
@@ -52,7 +53,7 @@
             if( ( $item['s_address'] != '' ) && ( $item['s_address'] != null ) ) { $addr[] = $item['s_address']; }
             if( ( $item['s_city'] != '' ) && ( $item['s_city'] != null ) ) { $addr[] = $item['s_city']; }
             if( ( $item['s_zip'] != '' ) && ( $item['s_zip'] != null ) ) { $addr[] = $item['s_zip']; }
-            if( ( $item['s_region'] != '' ) && ( $item['s_region'] != null ) ) { $addr[] = $item['s_region']; }
+            #if( ( $item['s_region'] != '' ) && ( $item['s_region'] != null ) ) { $addr[] = $item['s_region']; }
             if( ( $item['s_country'] != '' ) && ( $item['s_country'] != null ) ) { $addr[] = $item['s_country']; }
             $address = implode(", ", $addr);
         ?>
