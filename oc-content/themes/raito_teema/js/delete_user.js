64f1367aea18282fb649004769246add3999e35e
sugardrunk <jannekahkonen@gmail.com>
2016-04-10 16:34:36 +0300

    add delete user script for the user profile

---
+++ oc-content/themes/raito_teema/js/delete_user.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+	$(".opt_delete_account a").click(function() {
+		
+		$("#dialog-delete-account").dialog("open")
+	}),
+	$("#dialog-delete-account").dialog({
+		
+		autoOpen:!1,
+		modal:!0,
+		buttons:[{
+			text:raito_teema.langs["delete"],
+			click:function(){
+				window.location=raito_teema.base_url+"?page=user&action=delete&id="+raito_teema.user.id+"&secret="+raito_teema.user.secret
+			}
+		},
+		{
+			text:raito_teema.langs.cancel,click:function(){
+				$(this).dialog("close")
+			}
+		}]
+	})
+});
