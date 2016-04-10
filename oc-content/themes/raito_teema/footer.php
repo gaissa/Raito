aefcca4be5a401bf2134d04044541d12aacee938
sugardrunk <jannekahkonen@gmail.com>
2016-04-10 12:27:01 +0300

    add empty footer template

--- oc-content/themes/raito_teema/footer.php
+++ oc-content/themes/raito_teema/footer.php
@@ -26,59 +26,6 @@
 <?php osc_show_widgets('footer');?>
 
 <footer id="footer">
-  <div class="container">
-    <div class="footer">
-      <?php ?>
-      <?php if ( osc_count_web_enabled_locales() > 1) { ?>
-      <?php osc_goto_first_locale(); ?>
-      <strong>
-      <?php _e('Language:', raito_teema_THEME_FOLDER); ?>
-      </strong>
-      <ul>
-        <?php $i = 0;  ?>
-        <?php while ( osc_has_web_enabled_locales() ) { ?>
-        <li><a id="<?php echo osc_locale_code(); ?>" href="<?php echo osc_change_language_url ( osc_locale_code() ); ?>"><?php echo osc_locale_name(); ?></a></li>
-        <?php if( $i == 0 ) { echo ""; } ?>
-        <?php $i++; ?>
-        <?php } ?>
-      </ul>
-      <?php } ?>
-      <ul>
-        <?php if( osc_users_enabled() ) { ?>
-        <?php if( osc_is_web_user_logged_in() ) { ?>
-        <li> <?php echo sprintf(__('Hi %s', raito_teema_THEME_FOLDER), osc_logged_user_name() . '!'); ?> &#10072; <strong><a href="<?php echo osc_user_dashboard_url(); ?>">
-          <?php _e('My account', raito_teema_THEME_FOLDER); ?>
-          </a></strong> <a href="<?php echo osc_user_logout_url(); ?>">
-          <?php _e('Logout', raito_teema_THEME_FOLDER); ?>
-          </a> </li>
-        <?php } else { ?>
-        <li> <a href="<?php echo osc_user_login_url(); ?>">
-          <?php _e('Login', raito_teema_THEME_FOLDER); ?>
-          </a></li>
-        <?php if(osc_user_registration_enabled()) { ?>
-        <li> <a href="<?php echo osc_register_account_url(); ?>">
-          <?php _e('Register for a free account', raito_teema_THEME_FOLDER); ?>
-          </a> </li>
-        <?php } ?>
-        <?php } ?>
-        <?php } ?>
-        <?php
-        osc_reset_static_pages();
-        while( osc_has_static_pages() ) { ?>
-        <li> <a href="<?php echo osc_static_page_url(); ?>"><?php echo osc_static_page_title(); ?></a> </li>
-        <?php
-        }
-        ?>
-        <li> <a href="<?php echo osc_contact_url(); ?>">
-          <?php _e('Contact', raito_teema_THEME_FOLDER); ?>
-          </a> </li>
-        <?php if( osc_is_web_user_logged_in() && osc_users_enabled() || ( !osc_users_enabled() && !osc_reg_user_post() )) { ?>
-        <li class="publish"> <a href="<?php echo osc_item_post_url_in_category(); ?>">
-          <?php _e("Publish your ad for free", raito_teema_THEME_FOLDER);?>
-          </a> </li>
-        <?php } ?>
-      </ul>
-    </div>
-  </div>
+  
 </footer>
 <?php osc_run_hook('footer'); ?>
