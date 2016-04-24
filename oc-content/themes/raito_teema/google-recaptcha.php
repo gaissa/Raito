a84ece89e3e6c4d3e9dc3d8ed38b3a139cd9a23d
sugardrunk <jannekahkonen@gmail.com>
2016-04-24 12:16:51 +0300

    add google recaptcha code

---
+++ oc-content/themes/raito_teema/google-recaptcha.php
@@ -0,0 +1,51 @@
+<?php
+    /*
+     *      Osclass – software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2014 OSCLASS
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+
+
+    if( osc_recaptcha_public_key() ) {
+
+	?>
+
+         <script type="text/javascript">
+			var RecaptchaOptions = {
+				theme : 'custom',							
+				custom_theme_widget: 'recaptcha_widget'
+			};
+		</script>
+						
+        <style type="text/css"> 
+			div#recaptcha_widget, div#recaptcha_image > img { width:280px; } 
+	    </style>
+		
+          <div id="recaptcha_widget">
+		  
+            <div id="recaptcha_image"><img /></div>
+            <span class="recaptcha_only_if_image"><?php _e('Enter the words above',raito_teema_THEME_FOLDER); ?>:</span>
+			
+            <input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
+
+          </div>
+		  
+<?php
+
+    } 
+
+	osc_show_recaptcha(); ?>
